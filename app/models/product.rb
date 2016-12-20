39c233c24895e086ea09db2956298bba3bd609a8
Muhcoj <jochum.de.bueger@gmail.com>
2016-12-20 19:51:17 +0100

    5.7 Added comment model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,13 @@
 class Product < ApplicationRecord
 	# Product model has many orders
 	has_many :orders
+	has_many :comments
+
+	def highest_rating_comment
+		comments.rating_desc.first
+	end
+
+	def lowest_rating_comment
+		comments.rating_asc.first
+	end
 end
