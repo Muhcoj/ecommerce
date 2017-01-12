b96491e07998d363d15183f4050a5f40e8f6c39e
Muhcoj <jochum.de.bueger@gmail.com>
2017-01-12 16:31:13 +0100

    6.9 Added JQuery plugin ElevateZoom

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,9 +1,17 @@
 $(document).on('turbolinks:load', function(){
+	
 	$('.rating').raty( { path: '/images', scoreName: 'comment[rating]' });
+	
 	$('.rated').raty({ path: '/images',
 		readOnly: true,
 		score: function() {
-			return $(this).attr('data-score')
+			return $(this).attr('data-score');
 		}
-	})
-})
+	});
+	
+	//A jQuery image zoom plugin
+	$('.img-zoom').elevateZoom({
+  	zoomType: "inner",
+  	cursor: "crosshair"
+	});
+});
