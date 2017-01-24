fb4934bcd610e303b5b7ffe84c94eaace8f08dc4
Muhcoj <jochum.de.bueger@gmail.com>
2017-01-24 20:02:13 +0100

    6.14 Added ActionCable and ActiveJobs for comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -7,7 +7,9 @@ class Comment < ApplicationRecord
 
   #Because every comment record should be saved with a user ID, a product ID and a message, it's a good idea to validate the presence of all of those attributes
   validates :body, presence: true
-  	validates :user, presence: true
-  	validates :product, presence: true
-  	validates :rating, numericality: { only_integer: true }
+  validates :user, presence: true
+  validates :product, presence: true
+  validates :rating, numericality: { only_integer: true }
+
+  after_create_commit { CommentUpdateJob.perform_later(self, @user) }
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -4,10 +4,10 @@ class Product < ApplicationRecord
 	has_many :comments
 
 	validates :name, presence: true
-		validates :description, presence: true
-		validates :image_url, presence: true
-		validates :colour, presence: true
-		validates :price, presence: true
+	validates :description, presence: true
+	validates :image_url, presence: true
+	validates :colour, presence: true
+	validates :price, presence: true
 
 	def highest_rating_comment
 		comments.rating_desc.first
